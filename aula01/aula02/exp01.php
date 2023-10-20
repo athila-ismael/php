69526700c0eedff163bf7cd8c08b86b1d9cda723
athila-ismael <145491019+athila-ismael@users.noreply.github.com>
2023-10-19 23:22:58 -0300

    Add files via upload

---
+++ aula01/aula02/exp01.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel= "stylesheet" href = "exp01.css"
+
+</head>
+<body>
+    <pre> seletres  </pre>
+    <h1> titulo1 </h1>
+    <h2> sub titulo </h2>
+    <p> paragrafo </p>
+
+<pre> seletors com uso de id = para especificar um unico elemento </pre>
+<p id= "style1"> texto qualquer  </p>
+<p id= "style1"> titulo </p>
+
+
+    <p> paragrafo </p>
+    <p> paragrafo </p>
+    <p class= "style2">paragrafo</p>
+    <p class= "style2">paragrafo</p>
+    <p class= "style2">usa-se para valores elementos </p>
+
+</body>
+</html>
